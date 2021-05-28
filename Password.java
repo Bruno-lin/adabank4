d1ec13633bfa938bf70027336960068370998247
Bruno <woshibruno@gmail.com>
2021-05-28 15:54:04 +0800

    SHA512

--- Password.java
+++ Password.java
@@ -21,20 +21,44 @@ public class Password {
     /**
      * 计算哈希值，返回一个byte数组
      */
-    public static byte[] stringToBytes(String password){
+    public static byte[] stringToBytes(String password) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-512");
             return md.digest(password.getBytes());
-        } catch (NoSuchAlgorithmException e){
+        } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
             return null;
         }
     }
 
+    /**
+     * 解密算法
+     */
+    public static String decrypt(String digits, String encoded) {
+        int digit = Integer.parseInt(digits);
+
+        String Num = "9".repeat(digit);
+        int maxNum = Integer.parseInt(Num);
+
+        for (int i = 0; i <= maxNum; i++) {
+            byte[] bytes = stringToBytes(String.valueOf(i));
+            String str = bytesToHexString(Objects.requireNonNull(bytes));
+            if (str.equals(encoded)) return String.valueOf(i);
+        }
+        return "failed";
+    }
+
+
     public static void main(String[] args) {
-        String password = args[0];
-        byte[] bytes = stringToBytes(password);
-        String str = bytesToHexString(Objects.requireNonNull(bytes));
-        System.out.println(str);
+
+        if (args.length == 1) {
+            String password = args[0];
+            byte[] bytes = stringToBytes(password);
+            String str = bytesToHexString(Objects.requireNonNull(bytes));
+            System.out.println(str);
+        } else if (args.length == 2){
+            String decoded = decrypt(args[0], args[1]);
+            System.out.println(decoded);
+        }
     }
 }
