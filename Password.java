327725ff47f8f881a3627c97700aef9140856e4c
Bruno <woshibruno@gmail.com>
2021-05-29 23:19:32 +0800

    Letters

--- Password.java
+++ Password.java
@@ -1,9 +1,14 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 
 public class Password {
+
+    static String result;
 
     /**
      * 将一个byte数组转换成十六进制编码的字符串
@@ -35,30 +40,61 @@ public class Password {
     /**
      * 解密算法
      */
-    public static String decrypt(String digits, String encoded) {
+    public static void decrypt(String digits, String encoded, int threadNums) {
         int digit = Integer.parseInt(digits);
-        char[] alphabet = "0123456789abcdefghijklmnopqrstuvwxyz".toCharArray();
 
-        return findPassword(alphabet, new StringBuilder(), digit, 0, encoded);
+        char[] alphabet = "0123456789abcdefghijklmnopqrstuvwxyz".toCharArray();
+        List<char[]> lists = new ArrayList<>();
+        int range = alphabet.length / threadNums;
+
+        if (threadNums != 1) {
+            for (int i = 0; i < threadNums - 1; i++) {
+                lists.add(Arrays.copyOfRange(alphabet, i * range, (1 + i) * range));
+            }
+        }
+        lists.add(Arrays.copyOfRange(alphabet, (threadNums - 1) * range, alphabet.length));
+
+        CountDownLatch latch = new CountDownLatch(threadNums);
+        for (char[] chars : lists) {
+            new Thread(() ->
+            {
+                for (char c : chars) {
+                    findPassword(alphabet, new StringBuilder(String.valueOf(c)), digit, 1, encoded);
+                }
+                latch.countDown();
+            }
+            ).start();
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * 暴力破解过程
      */
-    public static String findPassword(char[] alphabet, StringBuilder sb,
-                                      int digit, int index, String encoded) {
+    public static void findPassword(char[] alphabet, StringBuilder sb,
+                                    int digit, int index, String encoded) {
+        if (digit == 1) {
+            if (isMatch(sb, encoded)) {
+                result = sb.toString();
+            }
+        }
+
         if (index < digit) {
             for (char c : alphabet) {
                 sb.append(c);
-
-                if (isMatch(sb, encoded)) return "found: " + sb;
-
+                if (isMatch(sb, encoded)) {
+                    result = sb.toString();
+                }
                 findPassword(alphabet, sb, digit, index + 1, encoded);
 
                 sb.deleteCharAt(sb.length() - 1);
             }
         }
-        return "failed";
     }
 
     //判断是否匹配
@@ -68,26 +104,26 @@ public class Password {
         return str.equals(encoded);
     }
 
-    public static void main(String[] args) throws InterruptedException {
-
+    public static void main(String[] args) {
         if (args.length == 1) {
             String password = args[0];
             byte[] bytes = stringToBytes(password);
             String str = bytesToHexString(Objects.requireNonNull(bytes));
             System.out.println(str);
         } else if (args.length == 2) {
-            String decoded = decrypt(args[0], args[1]);
-            System.out.println(decoded);
+            decrypt(args[0], args[1], 1);
+            print();
         } else if (args.length == 3) {
-            int threadNums = Integer.parseInt(args[2]);
-            CountDownLatch latch = new CountDownLatch(threadNums);
-            for (int i = 0; i < threadNums; i++) {
-                new Thread(() -> {
-                    System.out.println(decrypt(args[0], args[1]));
-                    latch.countDown();
-                }).start();
-            }
-            latch.await();
+            decrypt(args[0], args[1], Integer.parseInt(args[2]));
+            print();
         }
+    }
+
+    //打印
+    private static void print() {
+        if (result != null) {
+            System.out.println("found: " + result);
+        } else
+            System.out.println("failed");
     }
 }
